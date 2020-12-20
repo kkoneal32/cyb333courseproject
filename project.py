077947319da7f1a3a0db3b4f9a06af1c427848a2
K ONeal <k.oneal2146@student.nu.edu>
2020-12-19 18:03:14 -0800

    script for port scan & output text file

--- project.py
+++ project.py
@@ -1,2 +1,29 @@
-print("hello")
-print ("welcome to KO")
+#print("hello")
+#print ("welcome to KO")
+
+import socket
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = 'nu.edu.com'
+#nu.edu used as an example
+
+def pscan(port):
+    try:
+        s.connect((server,port))
+        return True
+    except:
+        return False
+
+for x in range(1,1000):
+    if pscan(x):
+        print('Port',x,'is open')
+
+sys.stdout = open('log.txt', 'w')
+print ('write to this file ')
+
+
+
+
+
+
